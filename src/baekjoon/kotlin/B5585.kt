79d0c1a33bc4bfa1197910ec3e9c550e4452fd4a
leetaehoon-android <lyc9290@39degreesc.com>
2021-08-02 20:44:37 +0900

    백준 5585 그리디

---
+++ src/baekjoon/kotlin/B5585.kt
@@ -0,0 +1,21 @@
+package baekjoon.kotlin
+
+fun main() {
+
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+
+    var changes = 1000 - br.readLine().toInt()
+    var cnt = 0
+
+    listOf(500, 100, 50, 10, 5, 1).forEach {
+        if (changes > it) {
+            cnt += changes / it
+            changes %= it
+        }
+    }
+
+    bw.write("$cnt\n")
+    bw.flush()
+    bw.close()
+}
